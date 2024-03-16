ba793fa63bb6c1fa6c5d5ce1dbd282c131dc4d60
TurabiOzturk <154705776+TurabiOzturk@users.noreply.github.com>
2024-03-16 21:29:53 +0300

    hello

---
+++ RunnersClub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RunnersClub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
